89fed0ac669963cb868869754a0be09f3510115a
Dario Abi Rached <dabi-rac@e3r2p8.42roma.it>
2024-09-13 12:41:06 +0200

    00 fin

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dabi-rac <dabi-rac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 16:45:18 by dabi-rac          #+#    #+#             */
-/*   Updated: 2024/09/13 12:36:36 by dabi-rac         ###   ########.fr       */
+/*   Updated: 2024/09/13 12:40:50 by dabi-rac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ int main(void)
 {
     try
     {
-        Bureaucrat franco("ganni", 200);
+        Bureaucrat franco("ganni", -12);
+        std::cout << franco << std::endl;
+        Bureaucrat franco("ganni", 12);
+        std::cout << franco << std::endl;
+        Bureaucrat franco("ganni", -122);
         std::cout << franco << std::endl;
     }
     catch(std::exception &e)
